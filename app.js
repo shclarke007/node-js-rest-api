971c0363b270c22a090798425a45f02ea143ec29
Shaneil Clarke <shaneilske@yahoo.com>
2019-04-09 22:45:30 +0100

    added conversations dummy database as /conversations route response

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const express = require('express');
+import express from 'express';
+import store from './db/store';
 const app = express();
 
 app.get('/conversations', (req, res) => {
   console.log('Responding to conversations route');
-  res.status(200).send('JSON file should go here');
+  res.status(200).send({
+    success: 'true',
+    message: 'conversations retrieved succesfully',
+    conversations: store
+  });
+});
+
+app.get('/messages', (req, res) => {
+  console.log('Responding to messages route');
+  res.status(200).send('JSON file for messages go here');
 });
 
 const PORT = 5000;
