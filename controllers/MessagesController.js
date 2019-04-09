1292a2a5486b07af4f8436b9b1c948f8e1e8e85c
Shaneil Clarke <shaneilske@yahoo.com>
2019-04-10 00:51:38 +0100

    added messages controller class with router methods

---
+++ controllers/MessagesController.js
@@ -0,0 +1,107 @@
+import store from '../db/messagesStore';
+
+class MessagesController {
+  getAllMessages(req, res) {
+    console.log('Responding to messages route');
+    res.status(200).send({
+      success: 'true',
+      message: 'messages retrieved succesfully',
+      messages: store
+    });
+  }
+
+  getMessage(req, res) {
+    const id = parseInt(req.params.id, 10);
+    store.map(msg => {
+      if (msg.id === id) {
+        return res.status(200).send({
+          success: 'true',
+          message: 'message retrieved successfully',
+          msg
+        });
+      }
+    });
+    return res.status(404).send({
+      success: 'false',
+      message: 'message does not exist'
+    });
+  }
+
+  deleteMessage(req, res) {
+    const id = parseInt(req.params.id, 10);
+    store.map((msg, index) => {
+      if (msg.id === id) {
+        store.splice(index, 1);
+        return res.status(200).send({
+          success: 'true',
+          message: 'message deleted successfuly'
+        });
+      }
+    });
+    return res.status(404).send({
+      success: 'false',
+      message: 'message not found'
+    });
+  }
+
+  createMessage(req, res) {
+    if (!req.body.text) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'text body is required'
+      });
+    }
+    const newMessage = {
+      id: store.length + 1,
+      text: req.body.text,
+      attachment: req.body.attachment
+    };
+    store.push(newMessage);
+    return res.status(201).send({
+      success: 'true',
+      message: 'message added successfully',
+      store
+    });
+  }
+
+  updateMessage(req, res) {
+    const id = parseInt(req.params.id, 10);
+    let messageFound;
+    let itemIndex;
+    store.map((msg, index) => {
+      if (msg.id === id) {
+        messageFound = msg;
+        itemIndex = index;
+      }
+    });
+
+    if (!messageFound) {
+      return res.status(404).send({
+        success: 'false',
+        message: 'message not found'
+      });
+    }
+    if (!req.body.text) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'text body is required'
+      });
+    }
+
+    const updatedMessage = {
+      id: messageFound.id,
+      text: req.body.text || messageFound.text
+    };
+
+    store.splice(itemIndex, 1, updatedMessage);
+
+    return res.status(201).send({
+      success: 'true',
+      message: 'message added successfully',
+      updatedMessage
+    });
+  }
+}
+
+const messageController = new MessagesController();
+export default messageController;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import conversationController from '../controllers/ConversationsController';
+import messageController from '../controllers/MessagesController';
 
 const router = express.Router();
 
@@ -8,5 +9,11 @@ router.get('/conversations/:id', conversationController.getConversation);
 router.post('/converstations', conversationController.createConversation);
 router.put('/conversations:id', conversationController.updateConversation);
 router.delete('/conversations/:id', conversationController.deleteConversation);
+
+router.get('/messages', messageController.getAllMessages);
+router.get('/messages/:id', messageController.getMessage);
+router.post('/messages', messageController.createMessage);
+router.put('/messages:id', messageController.updateMessage);
+router.delete('/messages/:id', messageController.deleteMessage);
 
 export default router;
