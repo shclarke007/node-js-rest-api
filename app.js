c66175d2d290622e47fd9fe034f3ba8db66523af
Shaneil Clarke <shaneilske@yahoo.com>
2019-04-10 00:16:21 +0100

    updated code with router middleware

--- app.js
+++ app.js
@@ -1,119 +1,12 @@
 import express from 'express';
-import store from './db/store';
+import router from './routes/index.js';
 import bodyParser from 'body-parser';
 const app = express();
 
-// Parse incoming requests data
-app.use(bodyParser.json());
+// Parse incoming requests data and use router middleware
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(router);
 
-app.get('/conversations', (req, res) => {
-  console.log('Responding to conversations route');
-  res.status(200).send({
-    success: 'true',
-    message: 'conversations retrieved succesfully',
-    conversations: store
-  });
-});
-
-app.get('/conversations/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  store.map((conversation) => {
-    if (conversation.id === id) {
-      return res.status(200).send({
-        success: 'true',
-        message: 'conversation retrieved successfully',
-        conversation,
-      });
-    } 
-  });
- return res.status(404).send({
-   success: 'false',
-   message: 'conversation does not exist',
-  });
-});
-
-app.delete('/conversations/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  store.map((conversation, index) => {
-    if (conversation.id === id) {
-       store.splice(index, 1);
-       return res.status(200).send({
-         success: 'true',
-         message: 'conversation deleted successfuly',
-       });
-    }
-  });
-    return res.status(404).send({
-      success: 'false',
-      message: 'conversation not found',
-    });
-});
-
-app.post('/conversations', (req, res) => {
-  if(!req.body.title) {
-    return res.status(400).send({
-      success: 'false',
-      message: 'title is required'
-    });
-  } 
- const conversation = {
-   id: store.length + 1,
-   title: req.body.title,
- }
- store.push(conversation);
- return res.status(201).send({
-   success: 'true',
-   message: 'conversation added successfully',
-   store
- })
-});
-
-app.put('/conversations/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  let conversationFound;
-  let itemIndex;
-  store.map((conversation, index) => {
-    if (conversation.id === id) {
-      conversationFound = conversation;
-      itemIndex = index;
-    }
-  });
-
-  if (!conversationFound) {
-    return res.status(404).send({
-      success: 'false',
-      message: 'conversation not found',
-    });
-  }
-  if (!req.body.title) {
-    return res.status(400).send({
-      success: 'false',
-      message: 'title is required',
-    });
-  }
-
-  const updatedConversation = {
-    id: conversationFound.id,
-    title: req.body.title || conversationFound.title
-  };
-
-  store.splice(itemIndex, 1, updatedConversation);
-
-  return res.status(201).send({
-    success: 'true',
-    message: 'conversation added successfully',
-    updatedConversation,
-  });
-});
-
-app.get('/messages', (req, res) => {
-  console.log('Responding to messages route');
-  res.status(200).send({
-    success: 'true',
-    message: 'messages retrieved succesfully'
-  });
-});
 
 const PORT = 5000;
 app.listen(PORT, () => {
