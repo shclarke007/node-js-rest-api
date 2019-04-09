b630931337aa48c1e3cdaaa61510b45471c990b3
Shaneil Clarke <shaneilske@yahoo.com>
2019-04-09 22:08:16 +0100

    created js object for dummy database

---
+++ db/store.js
@@ -0,0 +1,22 @@
+const conversations = [
+  {
+    id: 0,
+    title: 'Something about cats',
+    messages: {
+      id: 0,
+      text: 'My cat is here',
+      timeStamp: '2018-08-06'
+    }
+  },
+  {
+    id: 1,
+    title: 'Hello',
+    messages: {
+      id: 1,
+      text: 'Hi Mary',
+      timeStamp: '2018-04-06'
+    }
+  }
+];
+
+export default conversations;
