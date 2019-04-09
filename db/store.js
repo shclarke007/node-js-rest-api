0d7d0b604c349b60910caeec41df1a1eda78d1bb
Shaneil Clarke <shaneilske@yahoo.com>
2019-04-09 22:53:52 +0100

    updated dummy database with attachment property

--- db/store.js
+++ db/store.js
@@ -6,12 +6,12 @@ const conversations = [
       {
         id: 0,
         text: 'My cat is here',
-        timeStamp: '2018-08-06'
+        attachment: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjlpJz4_sPhAhUMoRQKHVJpDU0QjRx6BAgBEAU&url=https%3A%2F%2Fwww.cats.org.uk%2F&psig=AOvVaw0afZbIGeVdyk4hnOz0jjrE&ust=1554932993248128'
       },
       {
         id: 1,
         text: 'Yeah, I know',
-        timeStamp: '2018-08-06'
+        attachment: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjD3qaU_8PhAhWD3OAKHc4pD80QjRx6BAgBEAU&url=https%3A%2F%2Fwww.petplace.com%2Farticle%2Fcats%2Fpet-behavior-training%2Fhow-mother-cats-take-care-of-kittens%2F&psig=AOvVaw0afZbIGeVdyk4hnOz0jjrE&ust=1554932993248128'
       }
     ]
   },
@@ -21,7 +21,7 @@ const conversations = [
     messages: {
       id: 1,
       text: 'Hi Mary',
-      timeStamp: '2018-04-06'
+      attachment: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjg2Pig_8PhAhUPkxQKHUHkBBwQjRx6BAgBEAU&url=https%3A%2F%2Fwww.petfinder.com%2Fcat%2Fcashmere-rare-orange-female-38910278%2Fca%2Fsherman-oaks%2Fthe-fuzzy-pet-foundation-ca994%2F&psig=AOvVaw0afZbIGeVdyk4hnOz0jjrE&ust=1554932993248128'
     }
   }
 ];
